36255d7d1e61f8474d501e006835d942e1a9f001
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2022-11-05 09:57:44 +0100

    Refactor code from class components to functional component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,77 +1,73 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-    this.computeFunction = this.computeFunction.bind(this);
-  }
+const Calculator = () => {
+  const state = {
+    total: null,
+    next: null,
+    operation: null,
+  };
 
-  computeFunction = (e) => {
-    this.setState((now) => calculate({
+  const [now, toggle] = useState(state);
+
+  const computeFunction = (e) => {
+    toggle((now) => calculate({
       total: now.total,
       next: now.next,
       operation: now.operation,
     }, e.target.name));
-  }
+  };
 
-  render() {
-    // Destructuring this.state
-    const { total, next, operation } = this.state;
-    return (
-      <div className="panel">
-        <table>
-          <thead>
-            <tr className="result row">
-              <td colSpan={4}>
-                {total}
-                {operation}
-                {next}
-              </td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="row">
-              <td><button type="button" onClick={this.computeFunction} name="AC">AC</button></td>
-              <td><button type="button" onClick={this.computeFunction} name="+/-">+/-</button></td>
-              <td><button type="button" onClick={this.computeFunction} name="&#37;">%</button></td>
-              <td className="orange">
-                <button type="button" onClick={this.computeFunction} name="&#247;">&#247;</button>
-              </td>
-            </tr>
-            <tr className="row">
-              <td><button type="button" onClick={this.computeFunction} name="7">7</button></td>
-              <td><button type="button" onClick={this.computeFunction} name="8">8</button></td>
-              <td><button type="button" onClick={this.computeFunction} name="9">9</button></td>
-              <td className="orange"><button type="button" onClick={this.computeFunction} name="x">x</button></td>
-            </tr>
-            <tr className="row">
-              <td><button type="button" onClick={this.computeFunction} name="6">6</button></td>
-              <td><button type="button" onClick={this.computeFunction} name="5">5</button></td>
-              <td><button type="button" onClick={this.computeFunction} name="4">4</button></td>
-              <td className="orange"><button type="button" onClick={this.computeFunction} name="-">-</button></td>
-            </tr>
-            <tr className="row">
-              <td><button type="button" onClick={this.computeFunction} name="3">3</button></td>
-              <td><button type="button" onClick={this.computeFunction} name="2">2</button></td>
-              <td><button type="button" onClick={this.computeFunction} name="1">1</button></td>
-              <td className="orange"><button type="button" onClick={this.computeFunction} name="+">+</button></td>
-            </tr>
-            <tr className="row">
-              <td colSpan={2}><button type="button" onClick={this.computeFunction} name="0">0</button></td>
-              <td><button type="button" onClick={this.computeFunction} name=".">.</button></td>
-              <td className="orange"><button type="button" onClick={this.computeFunction} name="&#61;">=</button></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
+  // Destructuring state
+  const { total, next, operation } = now;
+  return (
+    <div className="panel">
+      <table>
+        <thead>
+          <tr className="result row">
+            <td colSpan={4}>
+              {total}
+              {operation}
+              {next}
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="row">
+            <td><button type="button" onClick={computeFunction} name="AC">AC</button></td>
+            <td><button type="button" onClick={computeFunction} name="+/-">+/-</button></td>
+            <td><button type="button" onClick={computeFunction} name="&#37;">%</button></td>
+            <td className="orange">
+              <button type="button" onClick={computeFunction} name="&#247;">&#247;</button>
+            </td>
+          </tr>
+          <tr className="row">
+            <td><button type="button" onClick={computeFunction} name="7">7</button></td>
+            <td><button type="button" onClick={computeFunction} name="8">8</button></td>
+            <td><button type="button" onClick={computeFunction} name="9">9</button></td>
+            <td className="orange"><button type="button" onClick={computeFunction} name="x">x</button></td>
+          </tr>
+          <tr className="row">
+            <td><button type="button" onClick={computeFunction} name="6">6</button></td>
+            <td><button type="button" onClick={computeFunction} name="5">5</button></td>
+            <td><button type="button" onClick={computeFunction} name="4">4</button></td>
+            <td className="orange"><button type="button" onClick={computeFunction} name="-">-</button></td>
+          </tr>
+          <tr className="row">
+            <td><button type="button" onClick={computeFunction} name="3">3</button></td>
+            <td><button type="button" onClick={computeFunction} name="2">2</button></td>
+            <td><button type="button" onClick={computeFunction} name="1">1</button></td>
+            <td className="orange"><button type="button" onClick={computeFunction} name="+">+</button></td>
+          </tr>
+          <tr className="row">
+            <td colSpan={2}><button type="button" onClick={computeFunction} name="0">0</button></td>
+            <td><button type="button" onClick={computeFunction} name=".">.</button></td>
+            <td className="orange"><button type="button" onClick={computeFunction} name="&#61;">=</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Calculator;
