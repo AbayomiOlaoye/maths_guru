797b7d6750620ca3be7661c0ed490b8d27b90141
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2022-11-05 08:41:53 +0100

    Implement Calculator logic for operations

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,52 +1,71 @@
 import React, { Component } from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.computeFunction = this.computeFunction.bind(this);
+  }
+
+  computeFunction = (e) => {
+    this.setState((now) => calculate({
+      total: now.total,
+      next: now.next,
+      operation: now.operation,
+    }, e.target.name));
   }
 
   render() {
+    // Destructuring this.state
+    const { total, next, operation } = this.state;
     return (
       <div className="panel">
         <table>
           <thead>
             <tr className="result row">
-              <td colSpan={4}>0</td>
+              <td colSpan={4}>
+                {total}
+                {operation}
+                {next}
+              </td>
             </tr>
           </thead>
           <tbody>
             <tr className="row">
-              <td>AC</td>
-              <td>+/-</td>
-              <td>%</td>
+              <td><button type="button" onClick={this.computeFunction} name="AC">AC</button></td>
+              <td><button type="button" onClick={this.computeFunction} name="+/-">+/-</button></td>
+              <td><button type="button" onClick={this.computeFunction} name="&#37;">%</button></td>
               <td className="orange">
-                <span className="semiC">:</span>
-                <span className="divide">-</span>
+                <button type="button" onClick={this.computeFunction} name="&#247;">&#247;</button>
               </td>
             </tr>
             <tr className="row">
-              <td>7</td>
-              <td>8</td>
-              <td>9</td>
-              <td className="orange">x</td>
+              <td><button type="button" onClick={this.computeFunction} name="7">7</button></td>
+              <td><button type="button" onClick={this.computeFunction} name="8">8</button></td>
+              <td><button type="button" onClick={this.computeFunction} name="9">9</button></td>
+              <td className="orange"><button type="button" onClick={this.computeFunction} name="x">x</button></td>
             </tr>
             <tr className="row">
-              <td>6</td>
-              <td>5</td>
-              <td>4</td>
-              <td className="orange">-</td>
+              <td><button type="button" onClick={this.computeFunction} name="6">6</button></td>
+              <td><button type="button" onClick={this.computeFunction} name="5">5</button></td>
+              <td><button type="button" onClick={this.computeFunction} name="4">4</button></td>
+              <td className="orange"><button type="button" onClick={this.computeFunction} name="-">-</button></td>
             </tr>
             <tr className="row">
-              <td>3</td>
-              <td>2</td>
-              <td>1</td>
-              <td className="orange">+</td>
+              <td><button type="button" onClick={this.computeFunction} name="3">3</button></td>
+              <td><button type="button" onClick={this.computeFunction} name="2">2</button></td>
+              <td><button type="button" onClick={this.computeFunction} name="1">1</button></td>
+              <td className="orange"><button type="button" onClick={this.computeFunction} name="+">+</button></td>
             </tr>
             <tr className="row">
-              <td colSpan={2}>0</td>
-              <td>.</td>
-              <td className="orange">=</td>
+              <td colSpan={2}><button type="button" onClick={this.computeFunction} name="0">0</button></td>
+              <td><button type="button" onClick={this.computeFunction} name=".">.</button></td>
+              <td className="orange"><button type="button" onClick={this.computeFunction} name="&#61;">=</button></td>
             </tr>
           </tbody>
         </table>
