c85ef05e1cad112ae274a8b95f37acf31a5c3b6c
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2022-11-11 15:42:29 +0100

    Implement minimal styling on MathMagicia

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,24 +1,26 @@
 import { Outlet, Link } from 'react-router-dom';
 
 const Header = () => (
-  <>
-    <h1>MathGician</h1>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/calculator">Calculator</Link>
-        </li>
-        <li>
-          <Link to="/quote">Quote</Link>
-        </li>
-      </ul>
-    </nav>
+  <div className="container">
+    <div className="flex-header">
+      <h1 style={{ color: 'cyan' }}>MathGician</h1>
+      <nav className="nav">
+        <ul style={{ display: 'flex', gap: '2rem' }} className="navList">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/calculator">Calculator</Link>
+          </li>
+          <li>
+            <Link to="/quote">Quote</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
 
     <Outlet />
-  </>
+  </div>
 );
 
 export default Header;
--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Quote = () => (
   <div>
+    <h2 className="quota">Random Quote</h2>
     <p className="paraText">
       Mathematics is not about numbers, equations, computations,
       algorithms: it is about understanding - William Paul Thurston
